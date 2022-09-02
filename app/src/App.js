5503fda246853a522329fdf58995915aa4ef6a85
Niten619 <nitenlama46@gmail.com>
2022-09-02 16:48:06 +0545

    added spl token payment

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,16 @@ import { useState } from "react";
 import {
   PublicKey, 
   Connection, 
-  clusterApiUrl
+  clusterApiUrl,
+  Keypair
 } from "@solana/web3.js";
 import "./styles.css";
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';  // THIS CAUSES "CRYPTO" MODULE ERROR UNLESS RESOLVED
 import { useWallet, WalletProvider, ConnectionProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { parseURL, createTransfer } from "@solana/pay";
+import { encodeURL, parseURL, createTransfer, createQR } from "@solana/pay";
+// import { QRCodeStyling} from "qr-code-styling";
+import BigNumber from "bignumber.js";
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const wallets = [
@@ -17,16 +19,14 @@ const wallets = [
   new PhantomWalletAdapter()
 ]
 
-const program_address = "3ERnPj1gQnKzagvWCjqZGskJszcLhunXuLfYqjuSvxBW";
-const programID = new PublicKey(program_address);
-console.log("programID:", programID)
+/* REPLACE THIS SENDER ADDRESS WITH YOUR WALLET ADDRESS FROM WHERE YOU WANT TO INITIATE A TRANSFER */
+const sender_wallet_ad = "HZpyGMxawAZy8ENvRtTsdQiwDGeBMtU2Dx1P9DFdRdvu";  // spl-wallet-1 of my Phantom
+const mercent_wallet_ad = "DqesRRc5fmXcG21HcgeWBsxryJmdYcerE8YzYHHJgoz1";
+const token_mint_ad = "2iB2oZaJZZBCmMecrz79wrMdu6Zn5UA2apUYdVy4jJUD";
 const opts = {
   preflightCommitment: "processed"
 }
 const network = clusterApiUrl('devnet');
-
-/* REPLACE THIS SENDER ADDRESS WITH YOUR WALLET ADDRESS FROM WHERE YOU WANT TO INITIATE A TRANSFER */
-// const sender_address = "3qupQgH5RaigtFNV7acr7Y9xvcc91F71VCf1szPH1Xej";  // Wallet 3 of my Phantom
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
@@ -41,12 +41,43 @@ function App() {
 
   
 async function solPay() {
+  /**
+   * Establish a connection
+   */
   const network = clusterApiUrl('devnet');
   const connection = new Connection(network, opts.preflightCommitment);
   
-  const url =
-    'solana:3qupQgH5RaigtFNV7acr7Y9xvcc91F71VCf1szPH1Xej?amount=1&reference=DqesRRc5fmXcG21HcgeWBsxryJmdYcerE8YzYHHJgoz1&label=Michael&message=Thanks%20for%20all%20the%20fish&memo=OrderId5678';
-  const { recipient, amount, splToken, reference, label, message, memo } = parseURL(url);
+  /**
+   * Gather customer checkout info's for url creation
+   */
+   const recipient = new PublicKey(mercent_wallet_ad);
+   const amount = new BigNumber(2);
+   const splToken = new PublicKey(token_mint_ad);
+   const reference = new PublicKey(sender_wallet_ad);
+   const label = 'Niten Daju Store';
+   const message = 'Niten Daju Store - Nick Shoes - #001234';
+   const memo = 'JC#4098';
+
+  /**
+   * Create a payment request link for native SOL transfer
+   */
+  // const url = encodeURL({ recipient, amount, reference, label, message, memo });
+  /**
+   * Create a payment request link for SPL Token transfer
+   */
+   const url = encodeURL({ recipient, amount, splToken, reference, label, message, memo });
+
+  // Transfer req URL scheme 
+  // const url =
+    // 'solana:3qupQgH5RaigtFNV7acr7Y9xvcc91F71VCf1szPH1Xej?amount=1&reference=DqesRRc5fmXcG21HcgeWBsxryJmdYcerE8YzYHHJgoz1&label=Michael&message=Thanks%20for%20all%20the%20fish&memo=OrderId5678';
+
+  console.log("url:", url)
+
+  /**
+   * We could also parse the url to get the respective parameters from the encoded url like this
+   */
+  // const { recipient, amount, splToken, reference, label, message, memo } = parseURL(url);
+
   console.log("recipient:", recipient)
   console.log("amount:", amount)
   console.log("splToken:", splToken)
@@ -54,24 +85,58 @@ async function solPay() {
   console.log("label:", label)
   console.log("message:", message)
   console.log("memo:", memo)
+
+
+/**
+ * Adding the QR code to the payment page
+ */
+  // Styling the QR code
+//   const qrCode = new QRCodeStyling({
+//     width: 300,
+//     height: 300,
+//     type: "svg",
+//     data: "https://www.facebook.com/",
+//     image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+//     dotsOptions: {
+//         color: "#4267b2",
+//         type: "rounded"
+//     },
+//     backgroundOptions: {
+//         color: "#e9ebee",
+//     },
+//     imageOptions: {
+//         crossOrigin: "anonymous",
+//         margin: 20
+//     }
+// });
+
+  // encode URL in QR code
+  const qrCode = createQR(url);
+
+  // get a handle of the element
+  const element = document.getElementById('qr-canvas');
+
+  // append QR code to the element
+  qrCode.append(element);
+
   
   /**
- * Create the transaction with the parameters decoded from the URL
+ * Set the payer and create the transaction with the required parameters
  */
 const payer = window.solana.publicKey;
 console.log("payer:", payer)
-const tx = await createTransfer(connection, payer, { recipient, amount, reference, memo });
+// const tx = await createTransfer(connection, payer, { recipient, amount, reference, memo });  // For Native SOL
+const tx = await createTransfer(connection, payer, { recipient, amount, splToken, reference, memo });  // For Spl Token
 console.log("tx:", tx)
+
+/**
+ * Sign the transaction and send it to the blockchain and then wait for the confirmation
+ */
 const signed = await window.solana.signTransaction(tx);
 const signature = await connection.sendRawTransaction(signed.serialize());
 console.log("signature:", signature)
 const finality = "confirmed";
 await connection.confirmTransaction(signature, finality);
-
-/**
- * Send the transaction to the network
- */
-//  sendAndConfirmTransaction(connection, tx, [CUSTOMER_WALLET]);
 
 }
 
@@ -89,8 +154,12 @@ const renderForm = (
       <div className="button-container">
         <input type="submit"  value='Send'/>
       </div>
+      <div id="qr-canvas">
+    
+      </div>
     </form>
   </div>
+  
 );
 
 // ADDED THIS CODE SNIPPET
